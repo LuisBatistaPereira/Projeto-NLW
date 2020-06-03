151e18fa303330f7c52c49373766d7e032917689
Luis <luisantoniobatistapereira@gmail.com>
2020-06-03 10:36:32 -0300

    Terminado o font-end do formulário

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,3 +1,4 @@
+//Adicionando cidades e estados as options
 function populateUFs(){
   //Adicionando o valor do selector uf a uma variavel
   const ufSelect = document.querySelector("select[name=uf]")
@@ -24,13 +25,17 @@ function getCities(event){
   const indexOfSelectedState = event.target.selectedIndex
   stateInput.value = event.target.options[indexOfSelectedState].text
 
+  //Resolvendo o erro das cidades aumentarem cada vez que tracava o estado
+  citySelect.innerHTML = "<option value>Selecione a cidade</option>"
+  citySelect.disabled = true
+
   fetch(url)
   //Convertendo em JSON()
   .then( res =>  res.json())
   .then( cities => {
-    for(city of cities){
+    for(city of cities){      
       //Adicionando novas options
-      citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+      citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
     }        
     citySelect.disabled = false
   })
@@ -38,3 +43,44 @@ function getCities(event){
 
 document.querySelector("select[name=uf]")
         .addEventListener("change", getCities) 
+
+
+// Itens de coleta selecionados
+const itensToCollect = document.querySelectorAll(".itens-grid li")
+
+//Percorrendo os items da class itens-grid
+for(item of itensToCollect){
+
+  item.addEventListener("click", handleSelectedItem)
+
+}
+
+const collectedItems = document.querySelector("input[name=items]")
+
+let selectedItems = []
+
+function handleSelectedItem(event){
+
+  const itemLi = event.target
+  
+  //Adicionar ou remover uma classe com JS
+  itemLi.classList.toggle("selected")
+
+  const itemId = itemLi.dataset.id
+
+  const alreadySelected = selectedItems.findIndex( item => item == itemId)
+
+  if(alreadySelected >= 0){
+    //Remover seleção
+    const filteredItems = selectedItems.filter(item => item != itemId)
+
+    selectedItems = filteredItems
+  }  
+  else{
+    //Adicionar elemento
+    selectedItems.push(itemId)
+  }
+
+  //Atualizar campo escondido com os itens selecionados
+  collectedItems.value = selectedItems
+}
